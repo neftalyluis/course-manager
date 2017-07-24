0e29d6e8f73f598f24677b39ba64ba41ae46bbe4
Abimael Luis <abi@airmail.cc>
2017-07-23 22:52:05 -0500

    Adding Spring Security plugin configurations

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import course.manager.PersonPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    personPasswordEncoderListener(PersonPasswordEncoderListener, ref('hibernateDatastore'))
 }
